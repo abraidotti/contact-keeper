2f09cb91617776c23ceaee97407465941e9fd606
abraidotti <alessandro.braidotti@gmail.com>
2020-01-07 16:14:40 -0500

    add server

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json({ message: "Welcome to the Contact Keeper API." });
-});
+// Connect Database
+connectDB();
 
-//routes
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
-app.use("/api/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
